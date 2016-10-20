dabc6a2b1c824eaee4753e4ec36aab3eb59a34f7
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-20 13:38:14 -0700

    add JS/jQuery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 //Buisness logic:
 
+
 //User Interface:
-$(function() {
-  
-}
+$(document).ready(function() {
+  $("form").submit(function(event) {
+  var reptile = $("#reptile").val();
+  var newReptile = parseInt(reptile);
+
+    if ( newReptile === 1 ) {
+      $("#snake").show();
+    }
+    else if (newReptile === 2) {
+      $("#beetle").show();
+    }
+    else if (newReptile === 3) {
+      $("#turtle").show();
+    }
+    else {
+      alert("Please select something");
+    }
+    event.preventDefault();
+  });
+});
